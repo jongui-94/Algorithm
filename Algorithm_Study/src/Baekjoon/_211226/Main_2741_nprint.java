53571c216a8b9ca2a9330988a5b0d131b14902a1
jongui2 <sa01070@gmail.com>
2021-12-26 00:28:55 +0900

    [211226]_[Baekjoon]

---
+++ Algorithm_Study/src/Baekjoon/_211226/Main_2741_nprint.java
@@ -0,0 +1,20 @@
+package Baekjoon._211226;
+
+import java.util.Scanner;
+
+public class Main_2741_nprint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		for(int i =1;i<=n;i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}

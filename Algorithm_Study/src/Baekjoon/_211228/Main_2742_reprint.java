7e63747db9f043dcc9381bd045cc05a2d4c895b7
jongui2 <sa01070@gmail.com>
2021-12-28 13:19:52 +0900

    [211228]_[Baekjoon]

---
+++ Algorithm_Study/src/Baekjoon/_211228/Main_2742_reprint.java
@@ -0,0 +1,21 @@
+package Baekjoon._211228;
+
+import java.util.Scanner;
+
+public class Main_2742_reprint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		
+		for(int i =n;i>0;i--)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}

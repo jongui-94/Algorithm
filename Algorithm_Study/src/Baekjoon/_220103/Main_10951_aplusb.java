7ff5223ced0aad7f438091467901cfb7cd7e2a12
jongui2 <sa01070@gmail.com>
2022-01-03 23:30:03 +0900

    [220103]_[Baekjoon]

---
+++ Algorithm_Study/src/Baekjoon/_220103/Main_10951_aplusb.java
@@ -0,0 +1,22 @@
+package Baekjoon._220103;
+
+import java.util.Scanner;
+
+public class Main_10951_aplusb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a, b;
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt())
+		{
+			a = sc.nextInt();
+			b = sc.nextInt();
+			System.out.println(a+b);
+		}
+		
+	}
+
+}
